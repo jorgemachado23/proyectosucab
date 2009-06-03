a11d8e1c449d9dc69d627da5de8505baee527da8
bioalexy <bioalexy@299fe774-4240-11de-875d-dfced7ea1f7d>
2009-06-03 01:59:57 +0000

    rmi

--- sistemas/Reserva Salones UCAB/src/Cliente/Encargado.java
+++ sistemas/Reserva Salones UCAB/src/Cliente/Encargado.java
@@ -1,10 +1,18 @@
 package Cliente;
 
+import java.rmi.Naming;
+import Servidor.InterfaceRMI;
+
 class Encargado extends Cliente
 {
+    public static void main(String[] args) {
+		try
+        {
+			InterfaceRMI h = (InterfaceRMI) Naming.lookup("rmi://localhost:1099/reserva");
+		} catch (Exception e)
+        {
+			e.printStackTrace();
+		}
 
-	public void AsignarSalon()
-    {
-	
 	}
 }
