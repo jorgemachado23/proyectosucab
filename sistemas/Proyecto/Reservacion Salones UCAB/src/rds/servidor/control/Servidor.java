0eeaaecfe55f063ca6c82214f0d1543cf137bbd9
bioalexy <bioalexy@299fe774-4240-11de-875d-dfced7ea1f7d>
2009-06-04 02:52:14 +0000

    Proyecto bonito

---
+++ sistemas/Proyecto/Reservacion Salones UCAB/src/rds/servidor/control/Servidor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rds.servidor.control;
+
+import java.net.InetAddress;
+import java.rmi.*;
+import java.rmi.registry.*;
+import java.rmi.server.*;
+import javax.swing.JFrame;
+import rds.general.vista.Notificar;
+import rds.servidor.vista.GUIServidor;
+import rds.servidor.xml.ManejoXML;
+import rds.servidor.conexion.jrmi.ImplementorRMI;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Servidor
+{
+
+}
