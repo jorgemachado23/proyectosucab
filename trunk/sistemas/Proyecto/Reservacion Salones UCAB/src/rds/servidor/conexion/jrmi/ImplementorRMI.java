7d6e6e8046e4d222416631879131a042776c5e59
justelnegro <justelnegro@299fe774-4240-11de-875d-dfced7ea1f7d>
2009-06-05 06:26:26 +0000

    cambie el puerto

--- trunk/sistemas/Proyecto/Reservacion Salones UCAB/src/rds/servidor/conexion/jrmi/ImplementorRMI.java
+++ trunk/sistemas/Proyecto/Reservacion Salones UCAB/src/rds/servidor/conexion/jrmi/ImplementorRMI.java
@@ -23,7 +23,7 @@ import java.sql.Time;
  */
 public class ImplementorRMI extends UnicastRemoteObject implements InterfaceRMI
 {
-    private Integer puerto = 3232;
+    private Integer puerto = 1099;
 
     public Integer getPuerto()
     {
