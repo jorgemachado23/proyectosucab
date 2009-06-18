ca009930cafb93c25c464b744a4bfd57704fb376
justelnegro <justelnegro@299fe774-4240-11de-875d-dfced7ea1f7d>
2009-06-18 01:30:44 +0000

    servidor con el manejo de hilos

--- sistemas/ajedrez/src/servidor/HiloServidor.java
+++ sistemas/ajedrez/src/servidor/HiloServidor.java
@@ -12,25 +12,88 @@ import java.util.*;
  *
  * @author Alejandro
  */
-public class HiloServidor extends Thread
+public  class  HiloServidor  implements  Runnable
 {
-    String jugador;
+     public Socket jugador;
+     public ServerSocket SocketCliente;
 
-     public HiloServidor(String jugador)
+
+     public HiloServidor(Socket jugador)
      {
-         this.jugador = jugador;
+        this.jugador = jugador;
      }
 
      @Override
-     public void run()
+     public synchronized  void run()
      {
             try
             {
-                System.out.println(jugador);
+//              if (!Servidor.usuariosConectados.contains(jugador.getInetAddress().getHostName()))
+//                 enviarConexion();
+//             else
+//                 recibirConexion();
+                enviarConexion();
+
             }
             catch(Exception e)
             {
                 System.out.println(e);
             }
+
      }
+     public void recibirConexion() throws IOException
+     {
+
+            Socket sock = jugador;
+
+            InputStream is = sock.getInputStream();
+
+            DataInputStream entrada = new DataInputStream( is );
+
+            System.out.println(entrada.readUTF());
+            
+            OutputStream fos = sock.getOutputStream();
+
+            DataOutputStream salida = new DataOutputStream( fos );
+
+            salida.writeUTF("\nSe recibio la respuesta del cliente " + sock.getInetAddress().getHostName());
+
+            is.close();
+
+            salida.close();
+
+            sock.close();
+
+     }
+public void enviarConexion() throws FileNotFoundException, IOException
+     {
+            Socket sock = jugador;
+
+            int puerto = Servidor.puerto;
+            
+            System.out.println("Coneccion de : " + sock.getInetAddress().getHostName() + " por el puerto " + puerto);
+
+            Servidor.usuariosConectados.add(sock.getInetAddress().getHostName());
+
+            InputStream fis =  jugador.getInputStream();
+
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            OutputStream os = sock.getOutputStream();
+            
+            DataOutputStream salida = new DataOutputStream (os);
+
+            System.out.println("Enviando respuesta al cliente" + sock.getInetAddress().getHostName());
+
+            salida.writeUTF("conetado");
+
+            System.out.println("Esperando respuesta del cliente" + sock.getInetAddress().getHostName());
+
+            os.flush();
+
+            os.close();
+
+            jugador.close();
+     }
+
 }
