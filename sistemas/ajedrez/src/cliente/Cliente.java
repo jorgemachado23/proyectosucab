cb47c73096f6b8e84cc3ceac11360bbad7ddb360
justelnegro <justelnegro@299fe774-4240-11de-875d-dfced7ea1f7d>
2009-06-18 01:30:20 +0000

    cliente

--- sistemas/ajedrez/src/cliente/Cliente.java
+++ sistemas/ajedrez/src/cliente/Cliente.java
@@ -9,66 +9,43 @@ public class Cliente {
 
     static final int PUERTO=5000;
 
-    static Socket skCliente = new Socket();
+    //static Socket skCliente = new Socket();
 
         public Cliente( ) {
 
-                    try{
-
-                    skCliente = new Socket( HOST , PUERTO );
-
-                    InputStream aux = skCliente.getInputStream();
-
-                    DataInputStream flujo = new DataInputStream( aux );
-
-                    System.out.println( flujo.readUTF() );
-
-                    }
-
-                    catch( Exception e ) {
-
-                    System.out.println( e.getMessage() );
-
-                    }
+                   
 
         }
 
-public void CerrarConexion()
+public void RecibirComando()
 {
-    try
-    {
-            skCliente.close();
-            System.out.println("Se ha cerrado la conexion con exito");
-    }
-    catch(Exception e)
-    {
+     try{
+
+            Socket skCliente = new Socket( HOST , PUERTO );
+
+            InputStream aux = skCliente.getInputStream();
+
+            DataInputStream flujo = new DataInputStream( aux );
+
+            System.out.println( flujo.readUTF() );
+
+            }
+
+            catch( Exception e ) {
 
             System.out.println( e.getMessage() );
-    }
 
-}
-public void EnviarComando()
-{
-    try{
-            OutputStream aux = skCliente.getOutputStream();
+            }
 
-            DataOutputStream flujo = new DataOutputStream( aux );
 
-            flujo.writeUTF("verga que ladilla");
-
-        }
-    catch(Exception e)
-    {
-    System.out.println( e.getMessage() );
-    }
-
-}
+      }
 
         public static void main( String[] arg ) {
 
         Cliente cliente = new Cliente();
-        cliente.EnviarComando();
-        cliente.CerrarConexion();
+
+        cliente.RecibirComando();
+       // cliente.CerrarConexion();
         
         //cliente.CerrarConexion();
         }
