7291aff14618489355b01d12cd677b50bb4af7a9
doryvazquez <doryvazquez@299fe774-4240-11de-875d-dfced7ea1f7d>
2010-06-05 22:38:12 +0000

    No es parte del proyecto de desarrollo

--- Desarrollo/Parte1 v2/borrarForo1.php
+++
@@ -1,9 +0,0 @@
-<script>
-		var x=window.confirm("¿Está seguro de que desea eliminar todo el contenido del foro?")
-		if (x)
-		{
-			window.location = "borrarForo.php";	
-		}
-		else
-		window.location = "gestionarForo.php";
-</script>
--- Desarrollo/Parte1 v2/eliminarAlumnoAdmin_2.php
+++
@@ -1,9 +0,0 @@
-<script>
-		var x=window.confirm("¿Eliminar todos los alumnos?")
-		if (x)
-		{
-			window.location = "eliminarAlumnoAdmin_3.php";	
-		}
-		else
-		window.location = "eliminarAlumnoAdmin_1.php";
-</script>	
