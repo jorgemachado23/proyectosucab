39f95b93dfba7edcce19f7f306da4d9c13fc8e44
doryvazquez <doryvazquez@299fe774-4240-11de-875d-dfced7ea1f7d>
2010-06-07 23:16:19 +0000

    Primera parte de Exámen Virtual, con sus respectivos links e interfaz. Se crean las preguntas para cada exámen virtual.

--- Desarrollo/aulaVirtual/apps/frontend/modules/sesion/templates/indexSuccess.php
+++ Desarrollo/aulaVirtual/apps/frontend/modules/sesion/templates/indexSuccess.php
@@ -83,7 +83,7 @@ if ($_SESSION["privilegio"]=="ADMIN"){
       </ul>
       <div class="newcomments">
         <ul>
-            <li><a href="">&nbsp;&nbsp;Agregar Exámen Virtual</a></li>
+            <li><a href="<?php echo url_for('cont_examen/evaluacion') ?>">&nbsp;&nbsp;Agregar Exámen Virtual</a></li>
             <li><a href="">&nbsp;&nbsp;Modificar Exámen Virtual</a></li>
             <li><a href="">&nbsp;&nbsp;Eliminar Exámen Virtual</a></li>
         </ul>
