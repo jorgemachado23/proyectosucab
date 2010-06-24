40f69eac7ec488bf2af7f0b67180834e0b187703
claudia1087@gmail.com <claudia1087@gmail.com@299fe774-4240-11de-875d-dfced7ea1f7d>
2010-06-24 17:32:35 +0000

    Detalles dl tema

--- trunk/Desarrollo/aulaVirtual/lib/form/base/BaseTemaForm.class.php
+++ trunk/Desarrollo/aulaVirtual/lib/form/base/BaseTemaForm.class.php
@@ -18,7 +18,7 @@ class BaseTemaForm extends BaseFormPropel
       'created_at'  => new sfWidgetFormDate(),
       'updated_at'  => new sfWidgetFormDate(),
       'descripcion' => new sfWidgetFormTextarea(),
-      'idpersona'   => new sfWidgetFormInput(),
+      'idpersona'   => new sfWidgetFormTextarea(),
     ));
 
     $this->setValidators(array(
